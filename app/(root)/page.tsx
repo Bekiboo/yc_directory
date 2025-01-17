bb4bb7c4c71c6484da625c253844244987233b21
Bekiboo <julien.connault@gmail.com>
2025-01-17 17:04:32 +0300

    Add StartupCard component and display startup posts on Home page; implement date formatting utility

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import SearchForm from '@/components/SearchForm'
+import StartupCard from '@/components/StartupCard'
 
 export default async function Home({
   searchParams,
@@ -6,6 +7,18 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>
 }) {
   const query = (await searchParams).query
+
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 100,
+      author: { _id: 1, name: 'John Doe' },
+      description: 'This is a description',
+      image: 'https://placehold.co/300x200',
+      category: 'Tech',
+      title: 'Startup Name',
+    },
+  ]
 
   return (
     <>
@@ -19,6 +32,23 @@ export default async function Home({
         </p>
 
         <SearchForm query={query} />
+      </section>
+
+      {/* Section Container */}
+      <section className="section_container">
+        <p className="text-3-semibold">
+          {query ? `Search results for ${query}` : 'All Startups'}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType, index: number) => (
+              <StartupCard key={index} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
       </section>
     </>
   )
